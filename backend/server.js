fd058818b930ae4af66186ad17d6d17df4ec66b2
Willey Nascimento <willeynascimentodev@waan.com.br>
2022-04-18 12:38:40 -0300

    routes and auth with jwt

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,14 @@
 const express = require('express')
+const colors = require('colors')
 const dotenv = require('dotenv').config()
 const PORT = process.env.PORT || 5000
 const { errorHandler } = require('./middleware/errorMidleware')
+const connectDB = require('./config/db')
+
+connectDB()
 
 const app = express()
+
 
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
